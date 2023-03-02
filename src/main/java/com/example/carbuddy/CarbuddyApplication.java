c3808bf056b9866a1497afdbbcbc8d629d473e66
hadrihl <hadrihl@outlook.com>
2023-03-02 11:42:22 +0800

    initial commit

---
+++ src/main/java/com/example/carbuddy/CarbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.carbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carbuddy/CarbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
